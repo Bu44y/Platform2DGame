8b173eaf385522631e59a22b63b8aebad117f775
Bu44y <chaiwatnasa@yahoo.com>
2025-08-23 16:22:09 +0700

    Score

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -2,13 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public static LevelManager instance;
     public int score;
-   
+
+    public Text scoreUI;
+
+
+    private void Start()
+    {
+        scoreUI.text = "Score = " + score.ToString();
+    }
+
 
     private void Awake()
     {
@@ -32,7 +41,7 @@ public class LevelManager : MonoBehaviour
     public void AddScore()
     {
         score += 10;
-        Debug.Log(score);
+        scoreUI.text = "Score = " + score.ToString();
     }
 
     
